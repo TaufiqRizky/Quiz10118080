9578c032dcb9a45ef0015b4874f5b152fff64589
TaufiqRizky <taufiq.it2@gmail.com>
2021-05-29 10:18:53 +0700

    Initial commit

---
+++ app/src/main/java/com/trizdev/quiz10118080/MainActivity.java
@@ -0,0 +1,20 @@
+package com.trizdev.quiz10118080;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//<!--tanggal pengerjaan : 29/05/2021-->
+//<!--Nim :10118080-->
+//<!--Nama :Taufiq Rizky-->
+//<!--Kelas : IF2/S1/VI-->
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
